c4d7ba1e29456d4c7c09294b106b2b5c798c24ae
NatanaelPimenta <143766182+NatanaelPimenta@users.noreply.github.com>
2023-10-31 23:38:27 -0300

    Add files via upload

---
+++ Gson.java
@@ -0,0 +1,14 @@
+package ListadeTarefas;
+
+import java.io.BufferedReader;
+import java.util.ArrayList;
+
+public class Gson {
+    public ArrayList<Tarefa> fromJson(BufferedReader br, Class<ArrayList> arrayListClass) {
+        return null;
+    }
+
+    public String toJson(ArrayList<Tarefa> tarefas) {
+        return null;
+    }
+}
